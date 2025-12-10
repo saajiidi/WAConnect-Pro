efe45671e4fbc795262d3cb4024cb7c4d607c71e
Sajid <saajiidi@gmail.com>
2025-12-10 13:16:18 +0600

    modified:   api/index.py modified:   templates/index.html

--- api/index.py
+++ api/index.py
@@ -1,1 +1,7 @@
+import os
+import sys
+
+# Add the parent directory to sys.path to allow imports from the root
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app import app
